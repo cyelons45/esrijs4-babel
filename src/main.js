6ae9de049c17d7625d435f40f3af2c71985e9e4c
samuel Nyarkoh <cyelons45@yahoo.com>
2019-10-12 04:45:13 -0400

    Adding Babel

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
-import EsriMap from "esri/Map";
-import MapView from "esri/views/MapView";
+import EsriMap from 'esri/Map';
+import MapView from 'esri/views/MapView';
 
-const map = new EsriMap({
-	basemap: "streets"
+setHeader ('Set-Cookie', 'HttpOnly;Secure;SameSite=Strict');
+
+var state;
+const map = new EsriMap ({
+  basemap: 'streets',
 });
 
-const view = new MapView({
-	container: "viewDiv",
-	map
+const view = new MapView ({
+  container: 'viewDiv',
+  map,
 });
+
+state = [];
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 23, 4, 4];
+
+data.forEach (function (el) {
+  state.push (el * 5);
+});
+console.log ();
